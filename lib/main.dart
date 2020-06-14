45945c05e8842db785df1b2a0eb404f1093cf8e7
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-06-14 11:10:44 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,57 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+String _name = 'amit';
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FriendlyChatApp();
+  }
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:MainPage() ,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  ChatMessage({this.text});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: Text(_name[0]),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
 
-class MainPage extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  //reading the contents of the input field and
+  //for clearing the field after the chat message is sent.
+
+  final _textController = TextEditingController();
+
+  ///this method clear the textfield  when enter is pressed
+  void _handleSubmitted(String text) {
+    print(text);
+    _textController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple[600],
-        title: Text('Flutter chatty'),
+        title: Text('FriendlyChat'),
       ),
-      body: Center(
-        child: Text('Learning flutter from codelabs'),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 8.0,
+      ),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: InputDecoration.collapsed(hintText: 'Send message'),
+            ),
+          ),
+          IconTheme(
+            data: IconThemeData(color: Theme.of(context).accentColor),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
